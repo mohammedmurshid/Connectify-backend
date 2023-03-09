dd62aab4f45c07f5fc9d08052331e865d7047677
Mohammed Murshid <murshid434@gmail.com>
2023-03-09 20:46:07 +0530

    follow and unfollow function

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -56,3 +56,63 @@ export const deleteUser = async (req, res) => {
     res.status(403).json("Access Denied! You can only delete your own profile");
   }
 };
+
+// follow a user
+export const followUser = async (req, res) => {
+  const id = req.params.id;
+
+  const { currentUserId } = req.body;
+
+  if (currentUserId === id) {
+    res.status(403).json("Action forbidder");
+  } else {
+    try {
+      const userToBeFollowed = await User.findById(id);
+      const followingUser = await User.findById(currentUserId);
+
+      if (!userToBeFollowed.followers.includes(currentUserId)) {
+        await userToBeFollowed.updateOne({
+          $push: { followers: currentUserId },
+        });
+        await followingUser.updateOne({
+          $push: { following: id },
+        });
+        res.status(200).json("Started following user");
+      } else {
+        res.status(403).json("You already following this person");
+      }
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  }
+};
+
+// unfollow a user
+export const unFollowUser = async (req, res) => {
+  const id = req.params.id;
+
+  const { currentUserId } = req.body;
+
+  if (currentUserId === id) {
+    res.status(403).json("Action forbidder");
+  } else {
+    try {
+      const userToBeUnFollowed = await User.findById(id);
+      const unFollowingUser = await User.findById(currentUserId);
+
+      if (userToBeUnFollowed.followers.includes(currentUserId)) {
+        await userToBeUnFollowed.updateOne({
+          $pull: { followers: currentUserId },
+        });
+        await unFollowingUser.updateOne({
+          $pull: { following: id },
+        });
+        res.status(200).json("Unfollowed user");
+      } else {
+        res.status(403).json("You are not following this person");
+      }
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  }
+};
