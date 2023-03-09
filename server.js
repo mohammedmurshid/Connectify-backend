efe8d4e0ce416758ba30e3c4f5adfab34dcc8b4c
Mohammed Murshid <murshid434@gmail.com>
2023-03-09 20:45:09 +0530

    user controller done

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import AuthRoute from "./routes/AuthRoute.js";
+import UserRoute from "./routes/UserRoute.js";
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 
 app.use("/auth", AuthRoute);
+app.use("/user", UserRoute);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server listening at Port ${process.env.PORT}`);
